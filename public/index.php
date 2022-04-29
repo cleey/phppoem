e8300b45a65621cd2a24839a2812af4594f43a57
lisihan <lisihan@outlook.com>
2022-04-29 17:15:03 +0800

    Fixed APP_PATH not finding path in CLI

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_PATH', realpath('../').'/app/');
+define('APP_PATH', realpath( __DIR__.'/../').'/app/');
 // 调试模式 上线关闭  off while online
 // 调试模式会每次重新编译view模板文件，关闭后只编译一次将提高性能
 // 关闭后，每次上线后需要清理重新 ../app/runtime 文件夹，这样新模板才会生效
